abf4c162b19054f72fc0eaaa5bc9327c5fb4ad0e
Tad <tad.3hatsmedia@gmail.com>
2018-09-04 20:59:32 -0500

    continue working after overcoming mysql connection errors

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('umami burger');
+INSERT INTO burgers (burger_name) VALUES ('quarter pounder with cheese');
+INSERT INTO burgers (burger_name) VALUES ('bacon whopper');
