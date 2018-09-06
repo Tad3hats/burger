cff42d0a96c7617e1e0d2a14d7a8b8827f3f853f
Tad <tad.3hatsmedia@gmail.com>
2018-09-05 22:16:43 -0500

    controllers update

--- controllers/burger_controllers.js
+++ controllers/burger_controllers.js
@@ -32,7 +32,7 @@ router.put("/api/burgers/:id", function(req, res) {
 
   console.log("condition", condition);
 
-  cat.update({
+  burger.update({
     eaten: req.body.eaten
   }, condition, function(result) {
     if (result.changedRows == 0) {
@@ -47,7 +47,7 @@ router.put("/api/burgers/:id", function(req, res) {
 router.delete("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
-  cat.delete(condition, function(result) {
+  burger.delete(condition, function(result) {
     if (result.affectedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
